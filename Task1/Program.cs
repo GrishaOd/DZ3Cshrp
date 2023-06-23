99130e25b2b2eaa78095061422a6f0fa99b83533
Grisha <odnokonnygrisha@gmail.com>
2023-06-23 10:46:35 +0300

    feat: FirstTask done

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿﻿﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+void PrintReversecheck(int number)
+{
+if (number / 10000 == number % 10 && number / 1000 % 10 == number % 100 / 10)
+{
+    Console.WriteLine("Данное число является палиндромом");
+}
+else
+{  
+    Console.WriteLine("Данное число не является палиндромом");
+}
+}
+
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 10000 || number > 99999)
+{
+    Console.WriteLine("Это не пятизначное число!");
+    return;
+}
+PrintReversecheck(number);
